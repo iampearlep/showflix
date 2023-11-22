e99f433fc6ee66a19aeb41fe54d6a6c49299c5fa
iampearlep <damilolaprincess4@gmail.com>
2023-11-22 23:08:50 +0100

    update movie list

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -4,43 +4,26 @@ import Card from './Card'
 import axios from 'axios'
 const MovieList = () => {
     const [movies, setMovies] = React.useState<any>([])
-    // async function getMovies(){
-    //     try{
-    //         const response = await axios.get(`${process.env.NEXT_PUBLIC_POPULAR_BASE_URL}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`)
-    //         setMovies(response.data.results)
-    //         console.log(movies)
-    //     }
-    //     catch(err){
-    //         console.log(err)
-    //     }
-    // }
-    // React.useEffect(() => {
-    //     getMovies()
-    // },[])
-    // React.useEffect(() => {
-    //     // Fetch popular movies from TMDb API
-    //     const fetchMovies = async () => {
-    //       try {
-    //         const response = await fetch(`${process.env.NEXT_PUBLIC_POPULAR_BASE_URL}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`)
-    //         if (!response.ok) {
-    //           throw new Error('Failed to fetch movies');
-    //         }
-    
-    //         const data = await response.json();
-    //         setMovies(data.results);
-    //       } catch (error) {
-    //         console.log(error);
-    //       }
-    //     };
-    
-    //     fetchMovies();
-    //   }, []);
-    
+
+    const getMovies = async () => {
+      try{
+        const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=92a541fb75d036a5d87a937dfe8f50d2')
+        setMovies(response.data)
+        console.log(response)
+        console.log(response.data)
+      } catch(err){
+        console.error(err)
+      }
+    }
+
+    React.useEffect(() => {
+           getMovies()
+     },[])
+  
   return (
     <div className='pt-10 md:pt-24 px-10'>
         <h2 className='text-2xl font-bold py-3'>Now Playing</h2>
          <div className='grid grid-cols-1 md:grid-cols-6 gap-x-6 gap-y-4'>
-     <Card />
      <Card />
      <Card />
      <Card />
