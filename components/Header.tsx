aa565e9e9375bf55100510768fa609770360c370
iampearlep <damilolaprincess4@gmail.com>
2023-11-23 23:58:46 +0100

    update components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
-
+import axios from 'axios'
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  overview: string;
+}
 const Header = () => {
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [randomMovie, setRandomMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try{
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_POPULAR_BASE_URL}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`)
+        setMovies(response.data.results)
+        const randomIndex = Math.floor(Math.random() * response.data.results.length);
+        setRandomMovie(response.data.results[randomIndex]);
+      } catch(err){
+        console.error(err)
+      }
+    }
+    getMovies()
+},[])
   return (
     <header>
       <div className='w-full -mt-[80px] md:-mt-[200px] md:h-[750px] overflow-y-hidden'>
-      <Image src={'https://image.tmdb.org/t/p/w1280/Ag3D9qXjhJ2FUkrlJ0Cv1pgxqYQ.jpg'} height={439} width={1280} priority={true} layout="responsive" alt='' className='brightness-50'/>
+      <Image src={`https://image.tmdb.org/t/p/w1280/${randomMovie?.poster_path}`} height={439} width={1280} priority={true} layout="responsive" alt='' className='brightness-50'/>
       </div>
       <div className='z-10 text-white absolute -mt-[380px] md:w-1/2 px-10'>
-     <h1 className='text-3xl font-bold'>The Avengers</h1>
-      <p className='py-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+     <h1 className='text-3xl font-bold'>{randomMovie?.title}</h1>
+      <p className='py-4'>{randomMovie?.overview}</p>
         <div className='flex flex-row gap-x-5'>
         <button className='bg-white rounded-md text-black text-sm font-bold py-2 px-4'>Watch Now</button>
         <button className='bg-black rounded-md text-white text-sm font-bold py-2 px-4'>My List</button>
