516918ed529943c7184427eba576b980708b2903
iampearlep <damilolaprincess4@gmail.com>
2023-11-28 20:54:28 +0100

    update components

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image'
 import Link from "next/link";
 import { Movie } from '@/types/type'
 
-const Card:React.FC<{ movie: Movie }> = ({ movie }) => {
+const Card: React.FC<{ movie: Movie }> = ({ movie }) => {
   return (
     <Link href='/' className=''>
-       <Image src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} height={750} width={500} priority={true} layout="responsive" alt='' className=''/>
-       <h4>{movie.title || movie.name}</h4>
+      <Image src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} height={750} width={500} priority={true} layout="responsive" alt='' className='' />
+      <h4>{movie.title || movie.name}</h4>
     </Link>
   )
 }
--- components/ui/MovieList.tsx
+++ components/ui/MovieList.tsx
@@ -14,32 +14,32 @@ const MovieList: React.FC<MovieListProps> = ({ movies }) => {
   return (
     <div>
       <Swiper
-      cssMode={true}
-      mousewheel={true}
-      keyboard={true}
-      modules={[ Mousewheel, Keyboard]}
-      breakpoints={{
-        200: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        300: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        600: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
-        700: {
-          slidesPerView: 5,
-          spaceBetween: 20,
-        },
-        900: {
-          slidesPerView: 6,
-          spaceBetween: 20,
-        },
-      }}
+        cssMode={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Mousewheel, Keyboard]}
+        breakpoints={{
+          200: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          300: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          600: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+          700: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+          },
+          900: {
+            slidesPerView: 6,
+            spaceBetween: 20,
+          },
+        }}
       >
         {movies.map((movie: Movie) => (
           <SwiperSlide key={movie.id}>
