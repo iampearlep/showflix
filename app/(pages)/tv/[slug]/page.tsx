f2e5640b22e790cf7ae106e9cf9cc7c1927de992
iampearlep <damilolaprincess4@gmail.com>
2024-01-08 12:32:08 +0100

    feat: update movie deails

--- app/(pages)/tv/[slug]/page.tsx
+++ app/(pages)/tv/[slug]/page.tsx
@@ -15,17 +15,17 @@ export default async function Page({ params }: { params: { slug: string } }){
      <div className='w-full h-screen absolute'>
       <Image src={`https://image.tmdb.org/t/p/w1280/${data.backdrop_path}`} height={750} width={500} priority={true}  alt='' className='w-full h-full brightness-50 blur-sm'/>
       </div>
-     <div className="text-white pt-10 pb-10 px-5 md:pt-12 md:pb-24 md:px-10 relative z-10">
-    <h2 className='text-2xl font-bold mt-10 md:mt-20 pb-14 text-center'>Movie Details</h2>
+     <div className="text-white pt-7 pb-10 px-5 md:pt-12 md:pb-24 md:px-10 relative z-10">
+    <h2 className='text-2xl font-bold mt-7 md:mt-20 pb-14 text-center'>Movie Details</h2>
      <div className=' flex flex-col-reverse md:flex-row-reverse gap-5 mx-auto'> 
-      <div className='w-full md:w-1/2 flex flex-col justify-center gap-y-3 items-start'> 
+      <div className='w-full md:w-1/2 flex flex-col justify-center gap-y-2 items-start'> 
         <h2 className='text-2xl font-bold'>{data.name}</h2>
-        <div className='flex flex-row flex-wrap gap-y-3 gap-x-5'>
+        <div className='flex flex-row flex-wrap gap-y-2 gap-x-5'>
           {data.genres.map((genre: any ) => {
             return <p key={genre.id} className='text-xs border border-yellow-800 px-4 py-2 rounded-full'>{genre.name}</p>
           })}
         </div>
-        <div className='flex flex-row flex-wrap gap-y-3 gap-x-5'>
+        <div className='flex flex-row flex-wrap gap-y-2 gap-x-5'>
           <p className='text-xs'><span className='text-gray-400'>Release Date:</span> {data.first_air_date}</p>
           <p className='text-xs'><span className='text-gray-400'>End Date:</span> {data.last_air_date}</p>
           <p className='text-xs'><span className='text-gray-400'>No of seasons:</span> {data.number_of_seasons}</p>
